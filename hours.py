60a62ef1e37f4d715a03c90d67395d7455ee9366
John Fitzpatrick <jfitz@computer.org>
2013-10-08 23:21:15 -0400

    add timesheet report

--- hours.py
+++ hours.py
@@ -1,7 +1,8 @@
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-import time, re, datetime
+import time, re
+import datetime
 from yastlib import *
 from parse_datetime import *
 import jinja2
@@ -10,51 +11,77 @@ import json
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-def parse_start_date(string_date):
+def parse_start_datetime(string_date):
 	if string_date != "":
 		date_s = parse_date(string_date)
-		date_s2 = time.mktime([date_s.year, date_s.month, date_s.day, 0, 0, 0, 0, 1, -1])
+		start_datetime = datetime.datetime(date_s.year, date_s.month, date_s.day)
 	else:
-		date_s2 = time.mktime([2012, 4, 1, 0, 0, 0, 0, 1, -1])
-	return date_s2
+		start_datetime = datetime.datetime(2012, 4, 1)
+	return start_datetime
 	
-def parse_end_date(string_date):
+def parse_end_datetime(string_date):
 	if string_date != "":
 		date_s = parse_date(string_date)
-		date_s2 = time.mktime([date_s.year, date_s.month, date_s.day, 23, 59, 59, 0, 1, -1])
+		end_datetime = datetime.datetime(date_s.year, date_s.month, date_s.day, 23, 59, 59)
 	else:
-		date_s2 = time.mktime(time.localtime())
-	return date_s2
+		end_datetime = datetime.today()
+	return end_datetime
 	
-def get_records_from_yast(yast, time_start, time_end, parent_id):
-	records = yast.getRecords({'timeFrom': time_start, 'timeTo': time_end, 'parentId': parent_id})
+def get_records_from_yast(yast, start_datetime, end_datetime, parent_id):
+	start_time = time.mktime([start_datetime.year, start_datetime.month, start_datetime.day, start_datetime.hour, start_datetime.minute, start_datetime.second, 0, 0, -1])
+	end_time = time.mktime([end_datetime.year, end_datetime.month, end_datetime.day, end_datetime.hour, end_datetime.minute, end_datetime.second, 0, 0, -1])
+	records = yast.getRecords({'timeFrom': start_time, 'timeTo': end_time, 'parentId': parent_id})
 	for k, r in records.iteritems():
 		time_start = r.variables['startTime']
-		r.variables['startDate'] = time.strftime('%Y-%m-%d', time.localtime(time_start))
+		r.variables['startDate'] = datetime.date(start_datetime.year, start_datetime.month, start_datetime.day)
 		r.variables['taskHours'] = str(round((r.variables['endTime'] - r.variables['startTime'])/3600.0, 2))
 	sorted_records = sorted(records.iteritems())
 	return sorted_records
 
-def summarize_records(records):
+def daterange(start_date, end_date):
+	for n in range(int((end_date - start_date).days) + 1):
+		my_datetime = start_date + datetime.timedelta(n) 
+		my_date = datetime.date(my_datetime.year, my_datetime.month, my_datetime.day)
+		yield my_date
+
+def get_summary_info(records):
+	summary_records = []
+	for k, r in records:
+		current_time = time.localtime(r.variables['startTime'])
+		current_date = datetime.date(current_time.tm_year, current_time.tm_mon, current_time.tm_mday) 
+		summary_records.append( { 'date': current_date, 'hours': float(r.variables['taskHours']) } )
+	return summary_records
+	
+def summarize_records(records, start_date, end_date):
 	summary_records = []
 	old_date = False
 	total_hours = 0.0
-	for k, r in records:
-		if r.variables['startDate'] != old_date:
+	for r in records:
+		current_datetime = r['date']
+		current_date = datetime.date(current_datetime.year, current_datetime.month, current_datetime.day) 
+		if current_date != old_date:
 			if old_date != False:
 				summary_records.append( { 'date': old_date, 'hours': total_hours } )
-			old_date = r.variables['startDate']
+			old_date = current_date
 			total_hours = 0.0
-		total_hours += float(r.variables['taskHours'])
+		total_hours += float(r['hours'])
 	if old_date != False:
 		summary_records.append( { 'date': old_date, 'hours': str(total_hours) } )
 	return summary_records
 	
-def get_projects_from_yast(yast, time_start, time_end, project_code):
+def get_projects_from_yast(yast, start_date, end_date, start_datetime, end_datetime, project_code):
 	projects = yast.getProjects()
-	sorted_records = get_records_from_yast(yast, time_start, time_end, project_code)
+	sorted_records = get_records_from_yast(yast, start_datetime, end_datetime, project_code)
 	yast_status = yast.getStatus()
-	summary_records = summarize_records(sorted_records)
+	if yast_status == 0:
+		complete_records = get_summary_info(sorted_records)
+		for single_date in daterange(start_date, end_date):
+			complete_records.append( { 'date': single_date, 'hours': 0 } )
+		complete_sorted_records = sorted(complete_records, key=lambda k: k['date'])
+		summary_records = summarize_records(complete_sorted_records, start_date, end_date)
+	else:
+		completed_sorted_records = []
+		summary_records = []
 	values = { 'status': yast_status, 'projects': projects, 'records': sorted_records, 'summary': summary_records }
 	return values
 			
@@ -77,36 +104,43 @@ class HoursReport(webapp.RequestHandler):
 	def __init__(self, *args, **kwargs):
 		super(HoursReport, self).__init__(*args, **kwargs)
 	
-	def get_yast_data(self, date_s2, date_e2, user_dict):
+	def get_yast_data(self, start_date, end_date, start_datetime, end_datetime, user_dict):
 		user = user_dict['user']
 		falabala = user_dict['fala'] + str(len(user_dict['fala'])) + user_dict['bala']
 		yast = Yast()
 		hash = yast.login(user, falabala)
 		if hash != False:
-			yast_dict = get_projects_from_yast(yast, date_s2, date_e2, 2015302)
+			yast_dict = get_projects_from_yast(yast, start_date, end_date, start_datetime, end_datetime, 2015302)
 			values = dict(user_dict.items() + yast_dict.items())
 			self.write_detail_response(values)
 		else:
 			self.response.out.write(yast_error(yast, self.error_template))
 
 	def get(self):
-		date_start = self.request.get('start_date')
-		date_end = self.request.get('end_date')
+		try:
+			start_datetime = datetime.datetime.strptime(self.request.get('start_date'), "%m/%d/%Y")
+			end_datetime = datetime.datetime.strptime(self.request.get('end_date'), "%m/%d/%Y")
+		except ValueError:
+			template_values = { }
+			self.response.out.write(self.date_error_template.render(template_values))
+			return
+		start_date = datetime.date(start_datetime.year, start_datetime.month, start_datetime.day)
+		end_date = datetime.date(end_datetime.year, end_datetime.month, end_datetime.day)
 		user = self.request.get('contractor_id')
 		fala = self.request.get('fala')
 		bala = self.request.get('bala')
 
 		try:
-			date_s2 = parse_start_date(self.request.get('start_date'))
-			date_e2 = parse_end_date(self.request.get('end_date'))
+			start_datetime = parse_start_datetime(self.request.get('start_date'))
+			end_datetime = parse_end_datetime(self.request.get('end_date'))
 		except ValueError:
 			template_values = { }
 			self.response.out.write(self.date_error_template.render(template_values))
 			return
 
-		user_dict = { 'user': user, 'fala': fala, 'bala': bala, 'start': date_start, 'end': date_end }
+		user_dict = { 'user': user, 'fala': fala, 'bala': bala, 'start': start_date, 'end': end_date }
 		# connect to yast.com and retrieve data
-		self.get_yast_data(date_s2, date_e2, user_dict)
+		self.get_yast_data(start_date, end_date, start_datetime, end_datetime, user_dict)
 
 class Timesheet(HoursReport):
 	def __init__(self, *args, **kwargs):
